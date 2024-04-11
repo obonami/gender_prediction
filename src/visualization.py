b959d819c95078d0844faad6eeb583d9dfe6bcd3
Nazanin <nazanin.shafiabadi97@gmail.com>
2024-04-11 13:56:34 +0200

    specified specific color for each class in the plots

--- src/visualization.py
+++ src/visualization.py
@@ -6,8 +6,9 @@ def plot_prediction_curve(word, predictions):
     class_names = list(predictions[0][1].keys())
     class_probs = [[tup[1][key] for tup in predictions] for key in class_names]
     characters = [tup[0] for tup in predictions]
+    colors = {'m': 'steelblue', 'f': 'green', 'b': 'orange'}
     for i, class_i in enumerate(class_probs):
-        plt.plot(range(len(characters)), class_i, label=class_names[i])
+        plt.plot(range(len(characters)), class_i, color=colors[class_names[i]], label=class_names[i])
     plt.title(f'Probability of each gender at each character position in "{word}"')
     plt.xlabel('Character indecies')
     plt.ylabel('Probability')
