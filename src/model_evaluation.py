9110dc092114a52d93d839cea63f13ffd3806664
Nazanin <nazanin.shafiabadi97@gmail.com>
2024-05-01 15:52:12 +0200

    added statistical checks

--- src/model_evaluation.py
+++ src/model_evaluation.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import matplotlib.pyplot as plt
 
 from data_processing import DataGenerator
@@ -34,49 +35,81 @@ def compare_accuracies(baseline_acc, model_acc):
     plt.show()
 
 
-def statistical_check(trainset, validset, hyperparameters, runs=10, reverse_nouns=True, device='cpu'):
+# def statistical_check(trainset, validset, hyperparameters, runs=10, reverse_nouns=True, device='cpu'):
+#     """
+#     Returns the accuracy, loss, plateau beginning index, and accuracy at plateau beginning index 
+#             averaged over a specified number of runs for both training and validation sets.
+#     """
+#     train_loss, train_acc, valid_loss, valid_acc = [], [], [], []
+#     train_idx_plateau_beg, train_acc_plateau_beg, valid_idx_plateau_beg, valid_acc_plateau_beg = [], [], [], []
+    
+#     embedding_dim = hyperparameters['embed_dim']
+#     hidden_size = hyperparameters['hidden_size']
+#     batch_size = hyperparameters['batch_size']
+#     n_epochs = hyperparameters['n_epochs']
+#     lr = hyperparameters['lr']
+
+#     for _ in range(runs):
+#         train_generator = DataGenerator(trainset, reverse_nouns=reverse_nouns)
+#         valid_generator = DataGenerator(validset, reverse_nouns=reverse_nouns)
+#         model = GenderLSTM(train_generator, embedding_dim, hidden_size, device=device)
+#         train, val = model.train_model(train_generator, valid_generator, n_epochs, batch_size, lr, verbose=False, save_model=False)
+        
+#         train_loss.append(train['loss'][-1])
+#         train_acc.append(train['accuracy'][-1])
+#         valid_loss.append(val['loss'][-1])
+#         valid_acc.append(val['accuracy'][-1])
+
+#         train_idx_plateau_beg.append(train['plateau_beg'][-1])
+#         train_acc_plateau_beg.append(train['acc_at_plateau_beg'][-1])
+#         valid_idx_plateau_beg.append(val['plateau_beg'][-1])
+#         valid_acc_plateau_beg.append(val['acc_at_plateau_beg'][-1])
+
+#     t = {'avg_accuracy': sum(train_acc) / len(train_acc),
+#          'avg_loss': sum(train_loss) / len(train_loss),
+#          'avg_plateau_beg': int(sum(train_idx_plateau_beg) / len(train_idx_plateau_beg)), 
+#          'avg_acc_at_plateau_beg': sum(train_acc_plateau_beg) / len(train_acc_plateau_beg)
+#         }
+            
+#     v = {'avg_accuracy': sum(valid_acc) / len(valid_acc),
+#          'avg_loss': sum(valid_loss) / len(valid_loss),
+#          'avg_plateau_beg': int(sum(valid_idx_plateau_beg) / len(valid_idx_plateau_beg)), 
+#          'avg_acc_at_plateau_beg': sum(valid_acc_plateau_beg) / len(valid_acc_plateau_beg)
+#         }
+
+#     print(f"Avg Train Loss: {t['avg_loss']:.4f}   Avg Train Accuracy: {t['avg_accuracy'] * 100:.2f}%   Avg Plateau Beginning (index): {t['avg_plateau_beg']}   Avg Plateau Beginning Accuracy: {t['avg_acc_at_plateau_beg'] * 100:.2f}%")      
+#     print(f"Avg Valid Loss: {v['avg_loss']:.4f}   Avg Valid Accuracy: {v['avg_accuracy'] * 100:.2f}%   Avg Plateau Beginning (index): {v['avg_plateau_beg']}   Avg Plateau Beginning Accuracy: {v['avg_acc_at_plateau_beg'] * 100:.2f}%")
+
+#     return t, v
+
+
+def statistical_check(trainset, validset, testset, hyperparameters, outfile, runs=10, reverse_nouns=True, device='cpu'):
     """
     Returns the accuracy, loss, plateau beginning index, and accuracy at plateau beginning index 
             averaged over a specified number of runs for both training and validation sets.
     """
-    train_loss, train_acc, valid_loss, valid_acc = [], [], [], []
-    train_idx_plateau_beg, train_acc_plateau_beg, valid_idx_plateau_beg, valid_acc_plateau_beg = [], [], [], []
-    
     embedding_dim = hyperparameters['embed_dim']
     hidden_size = hyperparameters['hidden_size']
     batch_size = hyperparameters['batch_size']
     n_epochs = hyperparameters['n_epochs']
     lr = hyperparameters['lr']
 
-    for _ in range(runs):
+    all_preds = []  # will contain the prediction info for each run as a pandas data frame
+    run_nums = []
+    for run in range(runs):
         train_generator = DataGenerator(trainset, reverse_nouns=reverse_nouns)
-        valid_generator = DataGenerator(validset, reverse_nouns=reverse_nouns)
-        model = GenderLSTM(train_generator, embedding_dim, hidden_size, device=device)
-        train, val = model.train_model(train_generator, valid_generator, n_epochs, batch_size, lr, verbose=False, save_model=False)
+        valid_generator = DataGenerator(validset, parentgenerator=train_generator, reverse_nouns=reverse_nouns)
+        test_generator = DataGenerator(testset, parentgenerator=train_generator, reverse_nouns=reverse_nouns)
         
-        train_loss.append(train['loss'][-1])
-        train_acc.append(train['accuracy'][-1])
-        valid_loss.append(val['loss'][-1])
-        valid_acc.append(val['accuracy'][-1])
+        model = GenderLSTM(train_generator, embedding_dim, hidden_size, device=device, reversed=reverse_nouns)
+        train, val = model.train_model(train_generator, valid_generator, n_epochs, batch_size, lr, verbose=False, save_model=False)
+        run_preds = model.predict(test_generator, batch_size, set='test')
+        all_preds.append(pd.DataFrame(run_preds))
+        run_nums.extend([str(run + 1)] * len(testset))
 
-        train_idx_plateau_beg.append(train['plateau_beg'][-1])
-        train_acc_plateau_beg.append(train['acc_at_plateau_beg'][-1])
-        valid_idx_plateau_beg.append(val['plateau_beg'][-1])
-        valid_acc_plateau_beg.append(val['acc_at_plateau_beg'][-1])
+    # Concatenate all DataFrames along the rows
+    final_df = pd.concat(all_preds, ignore_index=True)
+    final_df['Run'] = run_nums
+    final_df.to_csv(outfile, index=False)
 
-    t = {'avg_accuracy': sum(train_acc) / len(train_acc),
-         'avg_loss': sum(train_loss) / len(train_loss),
-         'avg_plateau_beg': int(sum(train_idx_plateau_beg) / len(train_idx_plateau_beg)), 
-         'avg_acc_at_plateau_beg': sum(train_acc_plateau_beg) / len(train_acc_plateau_beg)
-        }
-            
-    v = {'avg_accuracy': sum(valid_acc) / len(valid_acc),
-         'avg_loss': sum(valid_loss) / len(valid_loss),
-         'avg_plateau_beg': int(sum(valid_idx_plateau_beg) / len(valid_idx_plateau_beg)), 
-         'avg_acc_at_plateau_beg': sum(valid_acc_plateau_beg) / len(valid_acc_plateau_beg)
-        }
-
-    print(f"Avg Train Loss: {t['avg_loss']:.4f}   Avg Train Accuracy: {t['avg_accuracy'] * 100:.2f}%   Avg Plateau Beginning (index): {t['avg_plateau_beg']}   Avg Plateau Beginning Accuracy: {t['avg_acc_at_plateau_beg'] * 100:.2f}%")      
-    print(f"Avg Valid Loss: {v['avg_loss']:.4f}   Avg Valid Accuracy: {v['avg_accuracy'] * 100:.2f}%   Avg Plateau Beginning (index): {v['avg_plateau_beg']}   Avg Plateau Beginning Accuracy: {v['avg_acc_at_plateau_beg'] * 100:.2f}%")
-
-    return t, v
+    return f'Results successfully written to {outfile}'
--- src/visualization.py
+++ src/visualization.py
@@ -2,13 +2,20 @@ import ast
 import matplotlib.pyplot as plt
 
 
-def plot_prediction_curve(word, predictions):
+def plot_prediction_curve(word, predictions, true_class, binary=False):
+    """
+    If binary=True, plots the evolution of the true class probabilities over characters. 
+    Otherwise, plots the evolution of all class probabilities. 
+    """
     class_names = list(predictions[0][1].keys())
+    if binary:
+        class_names = [true_class]  # cleaner to only plot the evolution of the true class
     class_probs = [[tup[1][key] for tup in predictions] for key in class_names]
     characters = [tup[0] for tup in predictions]
     colors = {'m': 'steelblue', 'f': 'green', 'b': 'orange'}
+    plt.style.use('ggplot')
     for i, class_i in enumerate(class_probs):
-        plt.plot(range(len(characters)), class_i, color=colors[class_names[i]], label=class_names[i])
+        plt.plot(range(len(characters)), class_i, color=colors[class_names[i]], marker='x' , label=class_names[i])
     plt.title(f'Probability of each gender at each character position in "{word}"')
     plt.xlabel('Character indecies')
     plt.ylabel('Probability')
@@ -18,13 +25,15 @@ def plot_prediction_curve(word, predictions):
     print(f'Probability values:\n  {predictions}')
 
 
-def view_plateau(word, df):
+def view_plateau(word, df, binary=False):
     """
-    Checks to see if the word exists in the dataset and if so, plots the accuracy at each character position
+    Checks to see if the word exists in the dataset and if so, plots the class probabilities at each character position for each run
     """
     if word in df['Form'].to_list():
-        probabilities = df[df['Form'] == word]['Class Probabilities'].apply(lambda x: ast.literal_eval(x)).tolist()[0]
-        plot_prediction_curve(word, probabilities)
+        probabilities = df[df['Form'] == word]['Class Probabilities'].apply(lambda x: ast.literal_eval(x)).tolist()
+        true_class = df[df['Form'] == word]['True Gender'].iloc[0]
+        for run in probabilities:	
+            plot_prediction_curve(word, run, true_class=true_class, binary=binary)
 
     else:
         print(f'{word} not found.')
